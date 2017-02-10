ca5afb3f67beda67941f6e1c04cbbbf6c0416d78
Stephanus Andi Hermantyo <andi@andi1.andihermantyo.andi>
2017-02-10 17:39:59 +0700

    Java.gitignore testing : Success

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is Hello World from Java");
-        System.out.println("Java.gitignore testing");
+        System.out.println("Java.gitignore testing : Success");
     }
 }
